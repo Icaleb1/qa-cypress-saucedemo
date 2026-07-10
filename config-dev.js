081fda90dc98da77054fb4ab15a7d7c59edbe8f5
Icaleb1 <calebfernandes080@gmail.com>
2026-07-10 18:57:58 -0300

    ci: run Cypress tests in GitHub Actions using secrets

--- config-dev.js
+++ config-dev.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 const path = require('path')
 
 dotenv.config({
-    path: path.resolve(__dirname, './.env.dev')
+    path: path.resolve(__dirname, './.env')
 })
 
 const e2e = {
